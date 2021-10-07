4d16e549e9ba9b7c7cb4ee18cc4cfb94217e1ad5
Антон Корельский <korantonkor@list.ru>
2021-10-07 15:54:33 +0300

    Добавит компонент Header

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -1,5 +1,5 @@
 import CitiesCardScreen from '../cities-card-screen/cities-card-screen';
-import Logo from '../logo/logo';
+import Header from '../header/header';
 
 type MainScreenProps = {
   offersCount: number;
@@ -8,32 +8,7 @@ type MainScreenProps = {
 function MainScreen({offersCount}: MainScreenProps): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
--- project/src/components/not-found-screen/not-found-screen.tsx
+++ project/src/components/not-found-screen/not-found-screen.tsx
@@ -1,33 +1,9 @@
-import Logo from '../logo/logo';
+import Header from '../header/header';
 
 function NotFoundScreen(): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Page not found</h1>
         <section className="favorites favorites--empty">
@@ -35,7 +11,7 @@ function NotFoundScreen(): JSX.Element {
             <b className="favorites__status">404</b>
             <b className="favorites__status">PAGE NOT FOUND</b>
             <p>
-              <a href="/" className="header__nav-link header__nav-link--profile" style={{justifyContent: 'center'}}>
+              <a href="/">
                 Вернуться на главную
               </a>
             </p>
